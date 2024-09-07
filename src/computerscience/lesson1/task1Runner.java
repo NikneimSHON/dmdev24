28e635b030963d0096d715f6173ba234a399e27b
tsarp <tsarpkin.nmk22@yadenx.ru>
2024-09-07 15:22:34 +0300

    lesson1

---
+++ src/computerscience/lesson1/task1Runner.java
@@ -0,0 +1,43 @@
+package computerscience.lesson1;
+
+import java.util.Scanner;
+
+//Задание 1
+//
+//В переменной minutes лежит число от 0 до 59.
+//Написать функцию, которая принимает в качестве параметра значение переменной minutes и выводит на консоль
+//в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+//
+//Протестировать функцию в main.
+public class task1Runner {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите время");
+        int minutes = scan.nextInt();
+        if(isMinutesValid(minutes)){
+            isQuarterHour(minutes);
+        }
+        else{
+            System.out.println("Not a valid minutes");
+        }
+
+    }
+
+
+    public static void isQuarterHour(int minutes) {
+        if (minutes <= 15) {
+            System.out.println("The first part of the hour");
+        } else if (minutes <= 30) {
+            System.out.println("The second part of the hour");
+        } else if (minutes <= 45) {
+            System.out.println("The third part of the hour");
+        } else {
+            System.out.println("The fourth part of the hour");
+        }
+    }
+
+    public static boolean isMinutesValid(int minutes) {
+        return minutes >= 0 && minutes <= 59;
+    }
+}
---
+++ src/computerscience/lesson1/task3Runner.java
@@ -0,0 +1,49 @@
+package computerscience.lesson1;
+//Даны два прямоугольных треугольника.
+//Каждый из них задается двумя целочисленными переменными a и b - стороны треугольника.
+//Написать код, в котором вычисляется площадь каждого треугольника и затем эти площади сравниваются друг с другом.
+//
+//Для этого понадобится написать 2 функции.
+//Первая: по двум сторонам прямоугольного треугольника возвращает его площадь.
+//Вторая: сравнивает переданные площади двух треугольников и выводит на консоль первый треугольник больше, меньше или равен второму.
+//Учитывать, что площадь может быть вещественным числом.
+
+import java.util.Scanner;
+
+public class task3Runner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите сторону a для первого треугольника ");
+        int a1 = scan.nextInt();
+
+        System.out.println("Введите сторону b для первого треугольника ");
+        int b1 = scan.nextInt();
+
+        System.out.println("Введите сторону a для второго треугольника ");
+        int a2 = scan.nextInt();
+
+        System.out.println("Введите сторону b для второго треугольника ");
+        int b2 = scan.nextInt();
+
+        isComparisonAreas(isAreaTriangle(a1,b1), isAreaTriangle(a2,b2));
+
+
+    }
+
+    public static double isAreaTriangle(double side1, double side2) {
+        return side1 * side2 * 0.5;
+    }
+
+    public static void isComparisonAreas(double area1, double area2) {
+        if (area1 > area2) {
+            System.out.println("Площадь первого треугольника больше " + area1 + " > " + area2);
+        }
+        else if (area1 < area2) {
+            System.out.println("Площадь второго треугольника больше " + area1 + " < " + area2);
+        }
+        else{
+            System.out.println("Площадь треугольников равна" + area1 + " = " + area2);
+        }
+    }
+}
