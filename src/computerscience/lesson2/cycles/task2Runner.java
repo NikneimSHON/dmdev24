7d670b5d5f1bcdbe5d11b14eeb8dbb0ab49bb1a3
tsarp <tsarpkin.nmk22@yadenx.ru>
2024-09-12 17:21:18 +0300

    lesson1

--- src/computerscience/lesson2/cycles/task2Runner.java
+++ src/computerscience/lesson2/cycles/task2Runner.java
@@ -1,4 +1,30 @@
 package computerscience.lesson2.cycles;
 
+/**
+ * Задание 2
+ * Дано целое число.
+ * Написать функцию, которая принимает целое число, а возвращает целое число обратное этому (не строку!).
+ * Результат вывести на консоль.
+ * Например: 4508 -> 8054, 4700 -> 74, 1234567 -> 7654321
+ * <p>
+ * Примечание: для решения может понадобится функция возведение числа в степень: Math.pow(число, степень)
+ */
 public class task2Runner {
+    public static void main(String[] args) {
+        System.out.println(reverseNumber(-4700));
+
+
+    }
+
+    private static int reverseNumber(int number) {
+        int result = 0;
+        for (int currentValue = Math.abs(number); currentValue > 0; currentValue /= 10) {
+            result *= 10;
+            result += currentValue % 10;
+        }
+        return ((number > 0) ? result : result * -1);
+
+    }
+
 }
+
